91f84977a6cde41822d10e8c86a3320206b93d90
danielfulham1@yahoo.ie <danielfulham1@yahoo.ie>
2021-07-21 20:49:11 +0100

    Service Fabric tutorial 5

---
+++ NewServiceFabricApplication/FirstStatelessAPIService/Controllers/FirstController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstStatelessAPIService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FirstController : ControllerBase
+    {
+        [HttpGet]
+        [Route("get")]
+        public async Task Get()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
